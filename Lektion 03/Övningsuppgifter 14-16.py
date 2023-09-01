cd3442b82ac314e9990c308c69043a30d0559c70
Martin Lindberg <Martinlindberg@live.se>
2023-09-01 15:32:28 +0200

    Update Klar 010923

--- Lektion 03/Övningsuppgifter 14-16.py
+++ Lektion 03/Övningsuppgifter 14-16.py
@@ -138,9 +138,48 @@ if kön == "kvinna":
 
 
 
+# uppgift 4.1
+
+biggest_number = float('-inf')
+smallest_number = float('inf')
 
 
+while True:
+    user_input = input("Mata in ett tal: ")
+    user_input = int(user_input)
+    if int(user_input) < 0:
+        break
+    number_of_numbers =+ 1
+    sum =+ user_input
+    if user_input < smallest_number:
+        smallest_number = user_input
+    if user_input > biggest_number:
+        biggest_number = user_input
+if number_of_numbers > 0:
+    average = sum / number_of_numbers
+else: average = 0
+
+print("Största tal är: ", biggest_number)
+print("Minsta tal är: ", smallest_number)
+print("Medelvärdet är: ", average)
 
 
+# uppgift 4.2
+while True:
+    try:
+        multiplikationstabell = int(input("Ange multiplikationstabell (avsluta med 'nej'): "))
+        if multiplikationstabell < 0:
+            break
+        
+        for i in range(3):
+            resultat = multiplikationstabell * (i + 1)
+            print(resultat)
+        
+        fortsatt = input("Fortsätt? (ja/nej): ")
+        if fortsatt.lower() != 'ja':
+            break
+    except ValueError:
+        print("Ogiltig input. Försök igen.")
 
+print("Programmet avslutat.")
 
