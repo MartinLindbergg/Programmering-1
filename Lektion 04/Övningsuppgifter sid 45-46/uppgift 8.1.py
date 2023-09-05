f86d49a2d5d1951318d3431846d5dfd43d5ed72b
Martin Lindberg <Martinlindberg@live.se>
2023-09-05 22:39:20 +0200

    update 230905

---
+++ Lektion 04/Övningsuppgifter sid 45-46/uppgift 8.1.py
@@ -0,0 +1,22 @@
+'''
+#8.1
+todos = ["Städa", "Handla", "Plugga", "Ge blod"]
+print(todos[0])
+print(todos[2])
+print(todos[1])
+
+
+#8.2
+todos = ["Städa", "Handla", "Plugga", "Ge blod"]
+#Skriva ut ursprungslistan
+print(todos)
+
+#Inmatning av ny uppgift
+new_todo = input("Add a new task: ")
+#Lägger till nya uppgiften i todo listan
+todos.append(new_todo)
+print(todos)
+
+'''
+
+#8.3
