39f71bbc4fba883c06ff1f103b65cd1f5532081f
Martin Lindberg <Martinlindberg@live.se>
2023-09-05 12:35:49 +0200

    update 230905

--- Lektion 04/Övningsuppgifter sid 23-24/uppgift 5.1.py
+++ Lektion 04/Övningsuppgifter sid 23-24/uppgift 5.1.py
@@ -1,12 +1,15 @@
-
-indata = input ("Skriv in ett tal:")
-
+#inmatning
+indata = input ("Skriv in ett heltal tal:")
+#Funktion
 try:
+    #gör om indata till en integer
     indata = int(indata)
+    #gångrar inmatade talet med 2
     times_2 = indata * 2
+    #skriver ut inmatade talet och skriver ut det inmatade talet * 2
     print("Tal:" , indata)
     print("Resultat:", times_2)
-
+# felhantering, om inmatningen är annat än ett tal skriver programmet ut ett felmeddelande
 except ValueError:
     print(indata , "kan inte översättas till ett heltal")
 
