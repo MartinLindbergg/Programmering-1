87091d6ea47d61861ce6e2ffd3d2d1c2dba0d44b
Martin Lindberg <Martinlindberg@live.se>
2023-09-18 09:02:46 +0200

    update 230918

---
+++ Egna projekt/Kortspel21.py
@@ -0,0 +1,44 @@
+import random
+
+cards = []
+suits = ["spades", "clubs", "hearts", "diamonds"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit, rank])
+
+def shuffle():
+    random.shuffle(cards)
+
+def deal(number):
+    cards_dealt = []
+    for x in range(number):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+
+
+shuffle()
+cards_dealt = deal(2)
+card = cards_dealt[0]
+rank = card[1]
+
+if rank == "A":
+    value = 14
+
+elif rank == "J":
+    value = 11
+
+elif rank == "Q":
+    value = 12
+
+elif rank == "K":
+    value = 13
+
+else:
+    value = rank
+
+
+print(rank, value)
+
